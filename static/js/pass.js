f7fdaea9bda931974acc9ce500c58a0586107604
Joachim Wallsin <joachim@wallsin.com>
2014-12-13 21:23:09 +0100

    Ändrar i lösenordssidan.

--- static/js/pass.js
+++ static/js/pass.js
@@ -2,12 +2,14 @@ jQuery(function($){
 	$('#pass').submit(function(){
 		var identifier = $('#identifier').val();
 		var password = $('#password').val();
-		var hash_element = $('#hash');
 		var salt = '58756879c05c68dfac9866712fad6b63f8146f337a69afe7dd238f33649463';
 
 		var hash = CryptoJS.SHA256(identifier + password + salt);
 
-		hash_element.val(hash);
+		window.prompt("Kopiera till utklipp: Ctrl+C eller Enter", hash);
+
+		identifier.val();
+		password.val();
 	return false;
 	})
 })
