dd5b488a5dbd977c64fc97d19352e927af68ca28
Joachim Wallsin <joachim@wallsin.com>
2019-01-30 12:38:56 +0100

    About, portfolio, contact

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
     <Header/>
+    <div class="wrapper">
+      <About/>
+      <Portfolio/>
+      <Contact/>
+    </div>
     <Footer/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 export default {
   name: 'app',
   components: {
     Header,
+    About,
+    Portfolio,
+    Contact,
     Footer
   }
 }
---
+++ src/components/Contact.vue
@@ -0,0 +1,35 @@
+<template>
+  <section id="kontakt" class="contact">
+    <div class="row">
+      <div class="column">
+        <h1 class="section-header section-header--dark">Kontakt</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column small-12 text-center">
+        <p>Tveka inte att höra av er till mig.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column">
+        <div class="text-center">
+          <img src="/assets/joachim_edit-fcf6809e934bf38bd2667d450c64f28f0be59d86c48676701cd58fb948e074a2.jpg" class="small-4 medium-3 large-2 image-rounded image-rounded--border" />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column small-6 medium-3 medium-offset-3 text-center">
+        <p>E-postadress:<br /><a href="mailto:joachim@wallsin.com">joachim@wallsin.com</a></p>
+      </div>
+      <div class="column small-6 medium-3 end text-center">
+        <p>Telefonnummer:<br /><a href="tel:+46722525455">0722-52 54 55</a></p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
