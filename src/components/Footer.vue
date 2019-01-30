edc0b35b5c01790476ddd85b46d0ae979ce4c4ee
Joachim Wallsin <joachim@wallsin.com>
2019-01-30 12:30:24 +0100

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <div>
+      <img src="https://static.wallsin.com/html/logo.svg" class="small-4 medium-3 large-2 image-rounded image-rounded--separator" />
+    </div>
+    <div class="row">
+      <div class="column">
+        <div class="social">
+          <ul class="social__list">
+            <li class="social__list-item">
+              <a href="https://www.facebook.com/joachim.wallsin" class="social__list-link social__list-link--dark" target="_blank"><i class="fa fa-2x fa-facebook"></i></a>
+            </li>
+            <li class="social__list-item">
+              <a href="https://se.linkedin.com/in/joachim-wallsin-6b47a810" class="social__list-link social__list-link--dark" target="_blank"><i class="fa fa-2x fa-linkedin"></i></a>
+            </li>
+            <li class="social__list-item">
+              <a href="https://github.com/calvinmcgee" class="social__list-link social__list-link--dark" target="_blank"><i class="fa fa-2x fa-github"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="column text-center">Joachim Wallsin &copy; Copyright 2019</div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
