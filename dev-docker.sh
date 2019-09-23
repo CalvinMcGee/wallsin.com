6e3de412c5f05f9003f75f16d5bb6639fe6759a2
Joachim Wallsin <joachim.wallsin@fbinhouse.se>
2019-09-23 13:41:59 +0200

    Changing workdir

--- dev-docker.sh
+++ dev-docker.sh
@@ -7,5 +7,6 @@ echo "Please navigate to http://localhost:8080"
 docker run --init --rm \
 --mount type=bind,source="$(pwd)",target=/app \
 --entrypoint /bin/bash \
+-w="/app" \
 -p 8080:8080 \
 -it ruby:latest
